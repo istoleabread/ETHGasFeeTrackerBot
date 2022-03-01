b25bb3786315cd908f5989b950d68796c36008de
Advik Singh Somvanshi <github@advik.dev>
2022-03-02 00:14:37 +0530

    Fixed a bug

--- ethprice.py
+++ ethprice.py
@@ -2,21 +2,23 @@ import requests
 
 url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd%2Cbtc&include_market_cap=true&include_24hr_change=true"
 
-data = requests.get(url).json()
-pusd = round(data["ethereum"]["usd"], 2)
-pusdn = format(pusd, ",")
 
 def forgwei():
+    data = requests.get(url).json()
+    pusd = round(data["ethereum"]["usd"], 2)
     return pusd
 
 def getprice():
+    data = requests.get(url).json()
+    pusd = round(data["ethereum"]["usd"], 2)
+    pusd = format(pusd, ",")
     pbtc = round(data["ethereum"]["btc"], 8)
     pchange = round(data["ethereum"]["usd_24h_change"], 2)
     market = round(data["ethereum"]["usd_market_cap"])
     market = format(market, ",")
 	
     price = f"""<b><ins><a href='https://coingecko.com/en/coins/ethereum/'>Ethereum | $ETH</a> Price:</ins></b>
-<b>💰 USD:</b> ${pusdn}
+<b>💰 USD:</b> ${pusd}
 <b>🗿 BTC:</b> ฿{pbtc}
 <b>📈 24h change:</b> {pchange}%
 <b>💎 Market Cap:</b> ${market}
