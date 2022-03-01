3ee44e3bbdf0006b29b2481fd72c0c43dbd82f1c
Advik Singh Somvanshi <github@advik.dev>
2022-03-02 00:15:27 +0530

    Fixed a bug that won't update the data

--- gasfees.py
+++ gasfees.py
@@ -5,27 +5,27 @@ API = os.environ['ether']
 getgas = "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey="+API
 gweitoeth = 0.000000001
 
-data = requests.get(getgas).json()
+def gasfee():
+    data = requests.get(getgas).json()
 
-low = int(data["result"]["SafeGasPrice"])
-avg = int(data["result"]["ProposeGasPrice"])
-fast = int(data["result"]["FastGasPrice"])
-usdp = forgwei()
+    low = int(data["result"]["SafeGasPrice"])
+    avg = int(data["result"]["ProposeGasPrice"])
+    fast = int(data["result"]["FastGasPrice"])
+    usdp = forgwei()
 
-class feeusd:
-    def __init__(self, usd):
-        self.usd = str(round(usd * 21000 * usdp * gweitoeth, 2))
-
-lowp = feeusd(low)
-avgp = feeusd(avg)
-fastp = feeusd(fast)
-
-gasinfo = f"""<b><ins>Ethereum Live Gas Fees:</ins></b>
+    class feeusd:
+        def __init__(self, usd):
+            self.usd = str(round(usd * 21000 * usdp * gweitoeth, 2))
+    
+    lowp = feeusd(low)
+    avgp = feeusd(avg)
+    fastp = feeusd(fast)
+    
+    gasinfo = f"""<b><ins>Ethereum Live Gas Fees:</ins></b>
 Low: <i>{low}</i> GWEI | <i>${lowp.usd}</i>
 Average: <i>{avg}</i> GWEI | <i>${avgp.usd}</i>
 High/Fast: <i>{fast}</i> GWEI | <i>${fastp.usd}</i>
-
+    
 Powered by <a href=\"etherscan.io/gastracker\">EtherScan</a>."""
-
-def gasfee():
+    
     return gasinfo
